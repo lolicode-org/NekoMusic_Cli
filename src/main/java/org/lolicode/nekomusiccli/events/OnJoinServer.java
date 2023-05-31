7cbc0338636a6d096ecad5e4bd02e809182a2486
KoishiMoe <68314080+KoishiMoe@users.noreply.github.com>
2023-05-31 16:29:53 +0800

    dont send hello for disabled server

--- src/main/java/org/lolicode/nekomusiccli/events/OnJoinServer.java
+++ src/main/java/org/lolicode/nekomusiccli/events/OnJoinServer.java
@@ -2,11 +2,15 @@ package org.lolicode.nekomusiccli.events;
 
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.client.network.ServerInfo;
+import org.lolicode.nekomusiccli.NekoMusicClient;
 import org.lolicode.nekomusiccli.packet.ClientHelloSender;
 
 public class OnJoinServer {
     public static void OnPlayerJoin(MinecraftClient client) {
+        ServerInfo serverInfo = client.getCurrentServerEntry();
+        if (serverInfo == null || serverInfo.isLocal()) return;
+        if (!NekoMusicClient.config.enabled || NekoMusicClient.config.bannedServers.contains(serverInfo.address)) return;
         ClientHelloSender.send(client);
     }
 
