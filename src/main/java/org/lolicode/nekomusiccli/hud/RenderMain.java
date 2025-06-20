df50fab1516ab4dc7c653065b27a14aec9d0e158
KoishiMoe <68314080+KoishiMoe@users.noreply.github.com>
2025-06-21 03:27:30 +0800

    update render for 1.21.6

--- src/main/java/org/lolicode/nekomusiccli/hud/RenderMain.java
+++ src/main/java/org/lolicode/nekomusiccli/hud/RenderMain.java
@@ -1,18 +1,16 @@
 package org.lolicode.nekomusiccli.hud;
 
-import com.mojang.math.Axis;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.renderer.RenderType;
-import com.mojang.blaze3d.vertex.*;
+import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.resources.ResourceLocation;
-import org.joml.Matrix4f;
+import org.joml.Matrix3x2fStack;
 import org.lolicode.nekomusiccli.NekoMusicClient;
 
 public class RenderMain {
     private static final int fontHeight = Minecraft.getInstance().font.lineHeight;
     public static void drawText(GuiGraphics context, String text, float x, float y) {
-        context.drawString(Minecraft.getInstance().font, text, (int) x, (int) y, 0xffffff, false);
+        context.drawString(Minecraft.getInstance().font, text, (int) x, (int) y, 0xffffffff, false);
     }
 
     public static void drawMultiLineText(GuiGraphics context, String text, float x, float y) {
@@ -31,18 +29,16 @@ public class RenderMain {
         var imgSize = config.imgSize;
         var offset = imgSize / 2;
 
-        PoseStack matrices = context.pose();
-        matrices.pushPose();
-        Matrix4f matrix = matrices.last().pose();
+        Matrix3x2fStack matrices = context.pose();
+        matrices.pushMatrix();
 
+        matrices.translate(config.imgX + offset, config.imgY + offset);
         if (shouldRotate) {
-            matrix.translationRotate(config.imgX + offset, config.imgY + offset , 0, Axis.ZP.rotationDegrees(angle));
-        } else {
-            matrix.translate(config.imgX + offset, config.imgY + offset, 0);
+            matrices.rotate(angle * ((float)Math.PI / 180F));
         }
 
-        context.blit(RenderType::guiTextured, textureId, -offset, -offset, imgSize, imgSize, imgSize, imgSize, imgSize, imgSize);
+        context.blit(RenderPipelines.GUI_TEXTURED, textureId, -offset, -offset, imgSize, imgSize, imgSize, imgSize, imgSize, imgSize);
 
-        matrices.popPose();
+        matrices.popMatrix();
     }
 }
